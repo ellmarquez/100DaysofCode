e4d1078d1236accd1ba998a79ba3e178fa429cde
Ell Marquez <ellstripes@gmail.com>
2018-05-24 13:24:48 -0700

    T2 D3 Trying to understand the code logic.

--- Try2/WorkInProgress.py
+++ Try2/WorkInProgress.py
@@ -5,6 +5,7 @@ word= input ("Player 1: Please enter the word!").lower()
 lw= len(word)
 #os.system('clear')
 
+### Hands off to Player 2 
 print (""" Welcome to Hangman!  
              +---+
              |
@@ -15,30 +16,37 @@ print (""" Welcome to Hangman!
              
               """)
 print("The word has " + str(lw) + " letters.")
+print ("_ "*len(word))
 
-for x in range (0,5):
-    turn=0
-    guess = input ("Player 2: You can now guess the word or guess a Letter. \n What is your guess?").lower()
-    if guess == word:
-        print ("yaya you win!")
-        break
-    elif guess in (word):
-        print (guess + " is in the word!")
-        
-    elif guess not in (word):
-        turn+=1
-        print (guess + " is not in the word!")
-        if turn ==1:
-            from Hangmanboard import head
-            head()
-        elif turn ==2:
-            from Hangmanboard import body
-            body()
-        elif turn ==3:
-            from Hangmanboard import larm
-            larm()
-    else:
-        from Hangmanboard import rleg
-        rleg()
+### Decouple this section for guess function/class 
+### To do: add visible counter for user
+### Code above this line works 
+
+guess = input ("Player 2: You can now guess the word or guess a Letter. \n What is your guess?").lower()
+if guess == word:
+    print ("yaya you win!")
+    exit()
+elif guess != word:
+    for turn in range (1,6):
+        if guess in (word):
+            print (guess + " is in the word!")        
+        elif guess not in (word):
+            turn+=1
+            print (guess + " is not in the word!")
+            if turn==1:
+              from Hangmanboard import head
+              head()
+            elif turn ==2:
+                from Hangmanboard import body
+                body()
+            elif turn ==3:
+                from Hangmanboard import larm
+                larm()
+  
             
 
+
+
+elif turn ==5:
+          from Hangmanboard import rleg
+          rleg()   
